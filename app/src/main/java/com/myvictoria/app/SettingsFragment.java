282947e0e644275790d6c5fb9f4d863a5e7a7c41
Mayur <panchal.mayur2@gmail.com>
2016-02-20 15:58:08 +1300

    Added info section for the app.

--- app/src/main/java/com/myvictoria/app/SettingsFragment.java
+++ app/src/main/java/com/myvictoria/app/SettingsFragment.java
@@ -1,17 +1,32 @@
 package com.myvictoria.app;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Mayur on 3/05/2014.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(com.myvictoria.app.R.xml.preferences);
     }
 
-
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if(preference.getKey().equals("info")){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("This is an alert");
+            builder.setTitle("About");
+            builder.create();
+            builder.show();
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
 }
